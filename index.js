6575ebe2bf9a1b1645c091f89e3fd5c108e1ed9f
xsznix <xsznix@gmail.com>
2013-06-26 21:35:07 -0500

    Fix and optimize index.js
    I haven't tested this since I am unfamiliar with `component`, but I have an adapted version of this code working elsewhere.

--- index.js
+++ index.js
@@ -29,37 +29,32 @@ function PinkNoise (context, opts) {
 
 function pinkify (module) {
   var buffer = module.source.buffer,
-  b = [0,0,0,0,0,0,0], white, i, j, pink = [];
+  b = [0,0,0,0,0,0,0], channelData, white, i, j, pink = [];
   for (i = 0; i < module.channels; i++) {
-    pink[i] = [];
+    pink[i] = new Float32Array(module.bufferSize);
+    channelData = buffer.getChannelData(i);
     for (j = 0; j < module.bufferSize; j++) {
-      white = buffer.getChannelData(i)[j];
+      white = channelData[j];
       b[0] = 0.99886 * b[0] + white * 0.0555179;
       b[1] = 0.99332 * b[1] + white * 0.0750759;
-      b[2] = 0.96900 * b[2] + white + 0.1538520;
-      b[3] = 0.86650 * b[3] + white + 0.3104856;
-      b[4] = 0.55000 * b[4] + white + 0.5329522;
+      b[2] = 0.96900 * b[2] + white * 0.1538520;
+      b[3] = 0.86650 * b[3] + white * 0.3104856;
+      b[4] = 0.55000 * b[4] + white * 0.5329522;
       b[5] = -0.7616 * b[5] - white * 0.0168980;
-      pink[i][j] = b.reduce(sum, 0) + white * 0.5362;
+      pink[i][j] = b[0] + b[1] + b[2] + b[3] + b[4] + b[5] + b[6] + white * 0.5362;
       b[6] = white * 0.115926;
     }
     b = [0,0,0,0,0,0,0];
   }
+  
+  // Normalize to +/- 1 and prevent positive saturation
   var t = minMax(pink);
-  var min = t.min;
-  var max = t.max;
-
+  var coefficient = (2147483647 / 2147483648) / Math.max(Math.abs(t.min), t.max);
   pink.map(function (channel) {
-    return channel.map(function (val) {
-      // Normalize to +/- 1
-      if (Math.abs(min) > max) {
-        val /= Math.abs(min);
-      } else {
-        val /= max;
-      }
-      // Normalize to prevent positive saturation (cannot be 1.0)
-      return val / Math.abs((Math.pow(2,31) - 1) / Math.pow(2,31));
-    });
+    for (var k = 0; k < channel.length; k++) {
+      channel[i] = channel[i] * coefficient;
+    }
+    return channel;
   });
 
   for (i = 0; i < module.channels; i++) {
